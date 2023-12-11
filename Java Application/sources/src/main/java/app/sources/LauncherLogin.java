07d351e248119d4c21dd1ec0cc6d50eccb88d579
Franwik <franwik_@outlook.com>
2023-12-11 10:35:59 +0100

    feat[DB] Added Banck Account entity
    - Moved 'own' reletion from User to Person;
    - Added relation between Person and Bank Account;
    - Added relation between Card and Bank Account.
    
    Reviewed by @thankyousomaz

--- Java Application/sources/src/main/java/app/sources/LauncherLogin.java
+++ Java Application/sources/src/main/java/app/sources/LauncherLogin.java
@@ -13,7 +13,6 @@ public class LauncherLogin extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(LauncherLogin.class.getResource("Login.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.getIcons().add(new Image("file:icon.png"));
         stage.setTitle("Log In");
         stage.setScene(scene);
         stage.setResizable(false);
