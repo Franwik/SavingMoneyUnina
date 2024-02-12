82b35ae12a091be2b56705a92f91af5ad2e51711
Franwik <franwik_@outlook.com>
2024-02-12 12:19:45 +0100

    style[OO] changed default font to Arial - realigned register and login button; - added icon.

---
+++ Java Application/sources/saving-money-unina/src/main/java/DAOImplementation/UserDAOimp.java
@@ -0,0 +1,5 @@
+package DAOImplementation;
+
+public class UserDAOimp {
+
+}
--- Java Application/sources/saving-money-unina/src/main/java/smu/App.java
+++ Java Application/sources/saving-money-unina/src/main/java/smu/App.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import java.io.IOException;
 
@@ -16,6 +17,7 @@ public class App extends Application {
     public void start(@SuppressWarnings("exports") Stage stage) throws IOException {
         scene = new Scene(loadFXML("Login"), 700, 500);
         stage.setScene(scene);
+        stage.getIcons().add(new Image(getClass().getResourceAsStream( "icon.png" )));
         stage.setTitle("Saving Money Unina");
         stage.setResizable(false);
         stage.show();
