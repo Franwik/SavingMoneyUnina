430572dfc0add2b484cf0d2475659f53ca9a7c82
Franwik <franwik_@outlook.com>
2024-03-19 19:09:41 +0100

    fix[DB] fixed auto-connection trigger

--- Database/sources/SavingMoneyUnina.sql
+++ Database/sources/SavingMoneyUnina.sql
@@ -2,8 +2,8 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 16.2 (Postgres.app)
--- Dumped by pg_dump version 16.2 (Postgres.app)
+-- Dumped from database version 16.2 (Ubuntu 16.2-1.pgdg23.10+1)
+-- Dumped by pg_dump version 16.2 (Ubuntu 16.2-1.pgdg23.10+1)
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
