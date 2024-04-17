1be8865cfeee8722734eff2add353831ebe9cbc7
Franwik <franwik_@outlook.com>
2024-04-17 21:38:11 +0200

    fix[DB]: changed Database class to singleton

--- Java Application/sources/saving-money-unina/src/main/java/smu/Database.java
+++ Java Application/sources/saving-money-unina/src/main/java/smu/Database.java
@@ -4,20 +4,23 @@ import java.sql.*;
 
 public class Database {
 
-    public static Connection DBlink;
+    //Instance
+    private static Connection instance;
 
     public static Connection getConnection() throws SQLException{
- 
-        try {
 
-            Class.forName("org.postgresql.Driver");
-            DBlink = DriverManager.getConnection("jdbc:postgresql://localhost:5432/saving_money_unina", "postgres", "postgres");
-            
-        } catch (Exception e) {
-            e.printStackTrace();
+        if(instance == null){
+            try {
+
+                Class.forName("org.postgresql.Driver");
+                instance = DriverManager.getConnection("jdbc:postgresql://localhost:5432/saving_money_unina", "postgres", "postgres");
+                
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
 
-        return DBlink;
+        return instance;
     }
 
 }
